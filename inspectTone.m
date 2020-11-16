68c6ba127738981edce0fabf30f3166cbf581e6d
sivaprakasaman <andrew.n.sivaprakasam@gmail.com>
2020-11-15 22:47:40 -0500

    wrote progress report 1

--- inspectTone.m
+++ inspectTone.m
@@ -3,15 +3,18 @@
 clear all, close all
 
 addpath('Sound_Samples/Part A/')
+addpath('Sound_Samples/Part B/Tambourine')
+
 addpath('Functions')
 
 %for Part A:
 %instruments = ["banjo","bassoon","cello","clarinet","flute","oboe","trumpet","saxophone","viola","violin"];
-instruments = ["violin","viola","cello"]; %String subset
-%instruments = ["banjo","clarinet","flute","trombone","violin"];
-%instruments = ["trombone"];
+%instruments = ["violin","viola","cello"]; %String subset
+instruments = ["banjo","clarinet","flute","trombone","violin"];
+%instruments = ["tambourine"];
 pitch = 'A4';
 cond = 'normal';
+%cond = "long_cresc-decresc_shaken";
 timewindow = [0,1.0];
 nfft = 2048;
 
@@ -24,19 +27,19 @@ for i = 1:length(instruments)
     sig = sig(timewindow(1)*fs+1:timewindow(2)*fs);
     hold on
     plot(DFTfreq_Hz,DFTsig(i,:));
-    title(instruments(i));
     hold off
     sound(sig,fs);
-    getSpect(sig,40,fs,65,'mag',instruments(i))
-    cd Figures
-    saveas(gcf,strcat('spectrogram_', instruments(i)),'epsc')
-    cd ../
-    close all;
+%     getSpect(sig,40,fs,65,'mag',strcat(instruments(i),' ',' Crescendo_Hand'))
+%     cd Figures
+%     saveas(gcf,strcat('spectrogram_',instruments(i),'_',cond),'epsc')
+%     cd ../
+%     close all;
     pause(1);
     
 end
 
-% legend(instruments)
-% title("Frequency Response of A4 on Multiple Instruments");
-% xlim([0 max(DFTfreq_Hz)]);
-
+legend(instruments)
+title("Frequency Response of A4 on Multiple Instruments");
+xlim([0 max(DFTfreq_Hz)]);
+xlabel("Frequency (Hz)")
+ylabel("Amplitude")
